544daee234d0c77f42bdf1d77c7ba718d9e571b0
Egle2244 <h523624@yahoo.com.tw>
2025-07-03 16:14:02 +0800

    自動化測試1

---
+++ cathay_1.py
@@ -0,0 +1,16 @@
+"""這支程式會開啟國泰世華網站"""
+
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+mobile_emulation = { "deviceName": "iPhone X" }
+options = Options()
+options.add_experimental_option("mobileEmulation", mobile_emulation)
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://www.cathaybk.com.tw/cathaybk/")
+
+time.sleep(2)
+driver.save_screenshot("cathay_page1.png")  # 儲存為當前目錄的檔案
+driver.quit()
